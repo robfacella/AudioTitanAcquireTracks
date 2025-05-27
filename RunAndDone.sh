a48e246d0f2bdf8dd48c8944bcac0a4899531991
robfacella <rob.facella@gmail.com>
2025-05-26 20:55:44 -0400

    Comments

--- RunAndDone.sh
+++ RunAndDone.sh
@@ -1,1 +1,2 @@
+# Run and Remove
 bash runner.sh && cd .. && rm -Rf AudioTitanAcquireTracks/
--- runner.sh
+++ runner.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Generate Analysis Files and Directories
 bash AnalyzeTitan.sh
+# Run the Audio Track Extractions
 python3 TitanExtract.py3
